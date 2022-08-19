09ab9d56c3de5d55e95943b5cd348518753e5712
Andrew Yatskevich <drew.yatskevich@gmail.com>
2022-08-19 21:33:12 +0300

    add authentication app and custom user model

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,7 @@
 import os
 
+from datetime import timedelta
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,6 +23,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'authentication',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +70,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'authentication.User'
 
 # Password validation
 
@@ -101,3 +108,20 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+}
+
+SIMPLE_JWT = {
+    # Устанавливаем срок жизни токена
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+   'AUTH_HEADER_TYPES': ('Bearer',),
+}
---
+++ api_yamdb/authentication/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+
+from django.db import models
+
+ROLE_CHOICES = (
+    ('user', 'User'),
+    ('moderator', 'Moderator'),
+    ('admin', 'Admin')
+)
+
+
+class User(AbstractUser):
+    username = models.CharField('Пользователь', max_length=50, unique=True)
+    email = models.EmailField('Электронная почта', unique=True)
+    first_name = models.CharField('Имя', max_length=50, blank=True)
+    last_name = models.CharField('Фамилия', max_length=50, blank=True)
+    bio = models.TextField('Биография', max_length=200, blank=True)
+    role = models.CharField(
+        'Статус пользователя', choices=ROLE_CHOICES,
+        max_length=50, default='user'
+    )
+
+    def __str__(self):
+        return self.username
