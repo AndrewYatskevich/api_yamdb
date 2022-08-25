b8645f3ad65ee32f663deb5117d6578e33cad579
Nikita Viktorov <nikita.viktorov088@gmail.com>
2022-08-25 22:01:46 +1200

    Add serializers and views v1

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,12 +1,12 @@
-from xml.dom import ValidationErr
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.tokens import default_token_generator
 
 from rest_framework import serializers
+from api_yamdb.api_yamdb.reviews.models import Category, Genre
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from authentication.models import User
-from reviews.models import Comment, Review
+from reviews.models import Comment, Review, Title
 
 
 class SignUpSerializer(serializers.ModelSerializer):
@@ -82,3 +82,53 @@ class ReviewSerializer(serializers.ModelSerializer):
             if Review.objects.filter(title=title, author=author).exists():
                 raise serializers.ValidationError('Нельзя добавить более одного отзыва на произведение')
         return data
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        exclude = ('id',)
+        lookup_field = 'slug'
+        extra_kwargs = {
+            'url': {'looking_field': 'slug'}
+        }
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        exclude = ('id',)
+        lookup_field = 'slug'
+        extra_kwargs = {
+            'url': {'looking_field': 'slug'}
+        }
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        slug_field='slug', many=True, queryset=Genre.objects.all()
+    )
+    category = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Category.objects.all()
+    )
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+
+
+class ReadOnlyTitleSerializer(serializers.ModelSerializer):
+    rating = serializers.IntegerField(
+        source='reviews__score__avg', read_only=True
+    )
+    genre = GenreSerializer(many=True)
+    category = CategorySerializer()
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
+        
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+from django.db.models import Avg
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 
+from rest_framework import filters
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework import generics
@@ -10,6 +14,10 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .mixins import ListCreateDestroyViewSet
+from reviews.models import Category, Genre, Review, Title, User
+from .serializers import *
+from .filters import TitlesFilter
 
 from authentication.models import User
 from .serializers import (SignUpSerializer, YaMDbTokenObtainPairSerializer,
@@ -72,3 +80,34 @@ class UserViewSet(viewsets.ModelViewSet):
     pagination_class = PageNumberPagination
     filter_backends = (SearchFilter,)
     search_fields = ('=username',)
+
+
+class CategoryViewSet(ListCreateDestroyViewSet):
+    queryset = Category.objects.all()
+    serializers_class = CategorySerializer
+    filter_backends = (SearchFilter,)
+    serach_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class GenreViewSet(ListCreateDestroyViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ("name",)
+    lookup_field = "slug"
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all().annotate(
+        Avg("reviews__score")
+    ).order_by("name")
+    serializer_class = TitleSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TitlesFilter
+
+    def get_serializer_class(self):
+        if self.action in ('retrieve', 'list'):
+            return ReadOnlyTitleSerializer
+        return TitleSerializer
+
