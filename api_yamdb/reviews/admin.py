122ac7c27a8b68f8cb3c0221f6bb4250bb2f903c
Муслим Тасуев <ArchaonE@yandex.ru>
2022-08-27 17:23:59 +0300

    sort imports

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import *
+from .models import Category, Comment, Genre, GenreTitle, Review, Title
 
 admin.site.register(Category)
 admin.site.register(Genre)
