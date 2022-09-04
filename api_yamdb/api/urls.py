027191a20ea98de20f1e0423c127993acbee85e2
Муслим Тасуев <ArchaonE@yandex.ru>
2022-09-04 14:24:53 +0300

    dotenv

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -10,7 +10,7 @@ app_name = 'api'
 router_v1 = DefaultRouter()
 
 router_v1.register('users', UserViewSet, basename='user')
-router_v1.register(r'categories', CategoryViewSet)
+router_v1.register('categories', CategoryViewSet)
 router_v1.register(r'genres', GenreViewSet)
 router_v1.register(r'titles', TitleViewSet)
 router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
